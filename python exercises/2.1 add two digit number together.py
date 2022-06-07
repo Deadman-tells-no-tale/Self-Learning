8dca50a9bf3122533d19582c3b13fb3690ffd42d
Deadman-tells-no-tale <104101781+Deadman-tells-no-tale@users.noreply.github.com>
2022-06-06 22:20:38 -0600

    failed to commit in vscode
    trying to commit but i apparently can't? weird so upload necessary.

---
+++ python exercises/2.1 add two digit number together.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+first_number = list(two_digit_number)
+combine_a = int(first_number [0])
+combine_b = int(first_number [1])
+combine_together = (combine_a + combine_b)
+print (combine_together)
