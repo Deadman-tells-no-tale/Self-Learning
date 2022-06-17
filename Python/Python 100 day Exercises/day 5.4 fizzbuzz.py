71ef2e01a869075204782aa1cdbdbeed1b620564
Deadman-tells-no-tale <deadmankaos@gmail.com>
2022-06-16 21:42:47 -0600

    fizzbuzz game

--- Python/Python 100 day Exercises/day 5.4 fizzbuzz.py
+++ Python/Python 100 day Exercises/day 5.4 fizzbuzz.py
@@ -2,27 +2,12 @@
 #for anything divisible by 5 = buzz
 #for anything divisibile by both = fizz buzz
 
-from bdb import Breakpoint
-
-
-Count=100
-divthree = (Count % 3)
-print(divthree)
-divfive = (Count % 5)
-print(divfive)
-for Count in range(100):
-    
-    if divfive and divthree ==0:
-        Count -= 1
+for number in range(1,101):
+    if number % 3 ==0 and number %5 == 0:
         print("fizzbuzz")
-    elif divthree == 0:
-        print("fizz")
-        Count -= 1
-    elif divfive == 0:
+    elif number % 3 == 0:
+        print ("fizz")
+    elif number % 5 == 0:
         print("buzz")
-        Count -=1
-    
-else:
-    print(Count)
-    Count=0
-    
+    else:
+        print(number)
---
+++ Python/Python 100 day Exercises/day 5.5 Password Generator project.py
@@ -0,0 +1,6 @@
+#how many letter does user want
+#how many symbols does user want
+#how many numbers does user want
+
+Alphabit=["abcdefghijklmnopqrstuvwxyz"].split()
+print(Alphabit)
