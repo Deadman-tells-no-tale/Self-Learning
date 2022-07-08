6570651c8681869c5a54b9d1be9de13303e6125d
Deadman-tells-no-tale <deadmankaos@gmail.com>
2022-07-07 21:43:39 -0600

    attempt at hangman

--- Python/Python Projects/Hangman Game.py
+++ Python/Python Projects/Hangman Game.py
@@ -6,24 +6,25 @@ unsolved= ""
 
 #generate as many blanks as letters in the word
 #get a varible from user of a letter
-user_guess = input("What letter would you like to guess?:").lower
+
 #create an aski print to show the hangman
 #create a while loop that shows how many life is left
 #create a counter that detects how many lives/guesses are left
 #create a for loop to check if the letter in Range of the word
-guessed_word = ""
+guessed_word = []
 lives = 0
 while lives < 9:
-    
-    if guessed_word == winning_word:
+    user_guess = input("What letter would you like to guess?:").lower
+    if guessed_word == random_chose:
         print("Congratulations you won!")
+        print(f"You completed the phrase:",{guessed_word})
         break
-        for user_guess in winning_word:
-            if user_guess in random_chose:
+    elif guessed_word != random_chose:
+        for user_guess in random_chose:
                 for guess in range (0, len(random_chose)):
                     if user_guess in winning_word[guess]:
                         print(user_guess)
-                        guessed_word += user_guess
+                        guessed_word[guess] += user_guess
                     else:
                         print("_")
     else:
