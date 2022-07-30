9bca469605aba98397bf62ca65315ae6cd7875a6
Deadman-tells-no-tale <deadmankaos@gmail.com>
2022-07-29 20:13:00 -0600

    add new html forms and organization, new item test

--- Python/Python Projects/Material_burn.py
+++ Python/Python Projects/Material_burn.py
@@ -3,20 +3,24 @@ import time
 import random
 
 wood= ("oak, bark, birch, teak, mahogany, yew, apple")
-woodtype ={
-    "oak":1,
-    "bark":0.5,
-    "birch":3,
-    "teak":4,
-    "mahogany":2,
-    "yew":7,
-    "apple":2,
-}
-kindle_amount = input("How many logs are you placing in the furnace": )
-furnace_has_fuel = False
-Smoking = False
-heat = 0
-burn_length = time((woodtype * kindle_amount)
+class woodtype (name, density):
+    name ="oak"
+    density = 1
+    "bark":0.5
+    "birch":3
+    "teak":4
+    "mahogany":2
+    "yew":7
+    "apple":2
+
+kindle_amount = input("How many logs are you placing in the furnace:" )
+
+
+def furnace_in_use():
+    furnace_has_fuel = False
+    Smoking = False
+    heat = 0
+    burn_length = time(woodtype * kindle_amount)
 """
 need to create a furnace that burns fuel w time bassed off the type of woods
 player meeds to input how many wood pieces per wood 
